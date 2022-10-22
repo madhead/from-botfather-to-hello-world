9c9521545e278a48c144b327e0e1656812b09084
madhead <siarhei.krukau@gmail.com>
2022-10-22 02:46:48 +0200

    Echo bot

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,19 @@
-import dev.inmo.tgbotapi.extensions.api.send.sendTextMessage
+import dev.inmo.tgbotapi.extensions.api.send.reply
 import dev.inmo.tgbotapi.extensions.api.telegramBot
-import dev.inmo.tgbotapi.types.ChatId
+import dev.inmo.tgbotapi.extensions.utils.updates.retrieving.longPolling
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 suspend fun main(args: Array<String>) {
     val token = args[0]
     val bot = telegramBot(token)
 
-    bot.sendTextMessage(ChatId(254767265), "Hello, World!")
+    bot.longPolling {
+        messagesFlow
+            .onEach {
+                bot.reply(it.data, it.data)
+            }
+            .launchIn(GlobalScope)
+    }.join()
 }
