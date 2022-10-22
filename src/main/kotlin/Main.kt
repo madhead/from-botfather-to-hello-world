c3fd2b68aef3ccd097bac11d779596bb4df8022f
madhead <siarhei.krukau@gmail.com>
2022-10-22 02:38:28 +0200

    Printing incoming messages

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,16 @@
 import dev.inmo.tgbotapi.extensions.behaviour_builder.telegramBotWithBehaviourAndLongPolling
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
+import dev.inmo.tgbotapi.extensions.behaviour_builder.triggers_handling.onContentMessage
+import dev.inmo.tgbotapi.extensions.utils.extensions.raw.from
+import dev.inmo.tgbotapi.extensions.utils.extensions.raw.text
 
 suspend fun main(args: Array<String>) {
     val token = args[0]
     val (_, job) = telegramBotWithBehaviourAndLongPolling(token) {
-        this.allUpdatesFlow.onEach { println(it) }.launchIn(GlobalScope)
+        onContentMessage {
+            val user = it.from
+
+            println("${user?.firstName ?: "Unknown user"} wrote ${it.text}")
+        }
     }
 
     job.join()
