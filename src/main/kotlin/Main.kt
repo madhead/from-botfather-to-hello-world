81900a8e6adc1c967d8c3d210d758d7fc298ce3f
madhead <siarhei.krukau@gmail.com>
2022-10-22 23:51:00 +0200

    Scream / whisper mode

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,42 @@
 import dev.inmo.tgbotapi.extensions.api.send.reply
 import dev.inmo.tgbotapi.extensions.api.telegramBot
-import dev.inmo.tgbotapi.extensions.utils.updates.retrieving.longPolling
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
+import dev.inmo.tgbotapi.extensions.behaviour_builder.buildBehaviourWithLongPolling
+import dev.inmo.tgbotapi.extensions.behaviour_builder.triggers_handling.onCommand
+import dev.inmo.tgbotapi.extensions.behaviour_builder.triggers_handling.onContentMessage
+import dev.inmo.tgbotapi.extensions.utils.botCommandTextSourceOrNull
+import dev.inmo.tgbotapi.extensions.utils.extensions.raw.text
+import dev.inmo.tgbotapi.types.message.abstracts.CommonMessage
+import dev.inmo.tgbotapi.types.message.content.MessageContent
+import dev.inmo.tgbotapi.types.message.content.TextContent
 
 suspend fun main(args: Array<String>) {
     val token = args[0]
     val bot = telegramBot(token)
+    var screaming = false
 
-    bot.longPolling {
-        messagesFlow
-            .onEach {
-                bot.reply(it.data, it.data)
+    bot.buildBehaviourWithLongPolling() {
+        onCommand("scream") {
+            screaming = true
+        }
+        onCommand("whisper") {
+            screaming = false
+        }
+        onContentMessage(
+            initialFilter = CommonMessage<MessageContent>::isNotModeSwitchCommand
+        ) {
+            val text = it.text
+
+            if (text != null) {
+                bot.reply(it, if (screaming) text.uppercase() else text)
+            } else {
+                bot.reply(it, it)
             }
-            .launchIn(GlobalScope)
+        }
     }.join()
+}
+
+fun CommonMessage<*>.isNotModeSwitchCommand(): Boolean {
+    val content = this.content as? TextContent ?: return true
+
+    return content.textSources.none { it.botCommandTextSourceOrNull()?.command in listOf("scream", "whisper") }
 }
