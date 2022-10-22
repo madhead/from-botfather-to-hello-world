01e709eddb6b7c76fe339f7d733b880366553f67
madhead <siarhei.krukau@gmail.com>
2022-10-23 01:02:06 +0200

    Final version

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,29 +7,28 @@ import dev.inmo.tgbotapi.extensions.behaviour_builder.buildBehaviourWithLongPoll
 import dev.inmo.tgbotapi.extensions.behaviour_builder.triggers_handling.onCommand
 import dev.inmo.tgbotapi.extensions.behaviour_builder.triggers_handling.onContentMessage
 import dev.inmo.tgbotapi.extensions.behaviour_builder.triggers_handling.onDataCallbackQuery
+import dev.inmo.tgbotapi.extensions.behaviour_builder.triggers_handling.onMessageDataCallbackQuery
 import dev.inmo.tgbotapi.extensions.utils.botCommandTextSourceOrNull
-import dev.inmo.tgbotapi.extensions.utils.extensions.raw.message
 import dev.inmo.tgbotapi.extensions.utils.extensions.raw.text
 import dev.inmo.tgbotapi.types.buttons.InlineKeyboardButtons.CallbackDataInlineKeyboardButton
 import dev.inmo.tgbotapi.types.buttons.InlineKeyboardButtons.URLInlineKeyboardButton
 import dev.inmo.tgbotapi.types.buttons.InlineKeyboardMarkup
 import dev.inmo.tgbotapi.types.message.HTMLParseMode
 import dev.inmo.tgbotapi.types.message.abstracts.CommonMessage
-import dev.inmo.tgbotapi.types.message.abstracts.ContentMessage
 import dev.inmo.tgbotapi.types.message.content.MessageContent
 import dev.inmo.tgbotapi.types.message.content.TextContent
 
-const val firstMenu = "<b>Menu 1</b>\n\nA beautiful menu with a shiny inline button.";
-const val secondMenu = "<b>Menu 2</b>\n\nA better menu with even more shiny inline buttons.";
-val next = CallbackDataInlineKeyboardButton("Next", "next")
-val back = CallbackDataInlineKeyboardButton("Back", "back")
-val url = URLInlineKeyboardButton("Tutorial", "https://madhead.me/posts/from-botfather-to-hello-world")
-val firstMenuMarkup = InlineKeyboardMarkup(
+private val firstMenu = "<b>Menu 1</b>\n\nA beautiful menu with a shiny inline button.";
+private val secondMenu = "<b>Menu 2</b>\n\nA better menu with even more shiny inline buttons.";
+private val next = CallbackDataInlineKeyboardButton("Next", "next")
+private val back = CallbackDataInlineKeyboardButton("Back", "back")
+private val url = URLInlineKeyboardButton("Tutorial", "https://madhead.me/posts/from-botfather-to-hello-world")
+private val firstMenuMarkup = InlineKeyboardMarkup(
     listOf(
         listOf(next)
     )
 )
-val secondMenuMarkup = InlineKeyboardMarkup(
+private val secondMenuMarkup = InlineKeyboardMarkup(
     listOf(
         listOf(back),
         listOf(url),
@@ -45,9 +44,11 @@ suspend fun main(args: Array<String>) {
         onCommand("scream") {
             screaming = true
         }
+
         onCommand("whisper") {
             screaming = false
         }
+
         onCommand("menu") {
             bot.sendMessage(
                 chat = it.chat,
@@ -56,6 +57,7 @@ suspend fun main(args: Array<String>) {
                 replyMarkup = firstMenuMarkup
             )
         }
+
         onContentMessage(
             initialFilter = CommonMessage<MessageContent>::isNotCommand
         ) {
@@ -67,20 +69,32 @@ suspend fun main(args: Array<String>) {
                 bot.reply(it, it)
             }
         }
-        onDataCallbackQuery("next") {
-            bot.edit(message = it.message as ContentMessage<TextContent>, text = secondMenu, parseMode = HTMLParseMode, replyMarkup = secondMenuMarkup)
+
+        onMessageDataCallbackQuery("next") {
+            bot.edit(
+                chatId = it.message.chat.id,
+                messageId = it.message.messageId,
+                text = secondMenu,
+                parseMode = HTMLParseMode,
+                replyMarkup = secondMenuMarkup,
+            )
         }
-        onDataCallbackQuery("back") {
-            bot.edit(message = it.message as ContentMessage<TextContent>, text = firstMenu, parseMode = HTMLParseMode, replyMarkup = firstMenuMarkup)
+
+        onMessageDataCallbackQuery("next") {
+            bot.edit(
+                chatId = it.message.chat.id,
+                messageId = it.message.messageId,
+                text = firstMenu,
+                parseMode = HTMLParseMode,
+                replyMarkup = firstMenuMarkup,
+            )
         }
+
         onDataCallbackQuery {
             bot.answerCallbackQuery(it)
         }
     }.join()
 }
 
-fun CommonMessage<*>.isNotCommand(): Boolean {
-    val content = this.content as? TextContent ?: return true
-
-    return content.textSources.none { it.botCommandTextSourceOrNull() != null }
-}
+fun CommonMessage<*>.isNotCommand(): Boolean =
+    (this.content as? TextContent)?.textSources?.none { it.botCommandTextSourceOrNull() != null } ?: true
