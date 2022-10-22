30bade774ff353e01d150a5a14da4be47f6527f2
madhead <siarhei.krukau@gmail.com>
2022-10-23 00:03:50 +0200

    Menu

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,38 @@
 import dev.inmo.tgbotapi.extensions.api.send.reply
+import dev.inmo.tgbotapi.extensions.api.send.sendMessage
 import dev.inmo.tgbotapi.extensions.api.telegramBot
 import dev.inmo.tgbotapi.extensions.behaviour_builder.buildBehaviourWithLongPolling
 import dev.inmo.tgbotapi.extensions.behaviour_builder.triggers_handling.onCommand
 import dev.inmo.tgbotapi.extensions.behaviour_builder.triggers_handling.onContentMessage
 import dev.inmo.tgbotapi.extensions.utils.botCommandTextSourceOrNull
 import dev.inmo.tgbotapi.extensions.utils.extensions.raw.text
+import dev.inmo.tgbotapi.types.buttons.InlineKeyboardButtons.CallbackDataInlineKeyboardButton
+import dev.inmo.tgbotapi.types.buttons.InlineKeyboardButtons.URLInlineKeyboardButton
+import dev.inmo.tgbotapi.types.buttons.InlineKeyboardMarkup
+import dev.inmo.tgbotapi.types.message.HTMLParseMode
 import dev.inmo.tgbotapi.types.message.abstracts.CommonMessage
 import dev.inmo.tgbotapi.types.message.content.MessageContent
 import dev.inmo.tgbotapi.types.message.content.TextContent
+
+val next = CallbackDataInlineKeyboardButton("Next", "next")
+val back = CallbackDataInlineKeyboardButton("Back", "back")
+val url = URLInlineKeyboardButton("Tutorial", "https://madhead.me/posts/from-botfather-to-hello-world")
 
 suspend fun main(args: Array<String>) {
     val token = args[0]
     val bot = telegramBot(token)
     var screaming = false
+    val keyboardM1 = InlineKeyboardMarkup(
+        listOf(
+            listOf(next)
+        )
+    )
+    val keyboardM2 = InlineKeyboardMarkup(
+        listOf(
+            listOf(back),
+            listOf(url),
+        )
+    )
 
     bot.buildBehaviourWithLongPolling() {
         onCommand("scream") {
@@ -21,8 +41,15 @@ suspend fun main(args: Array<String>) {
         onCommand("whisper") {
             screaming = false
         }
+        onCommand("menu") {
+            bot.sendMessage(
+                chat = it.chat,
+                text = "<b>Menu 1</b>",
+                parseMode = HTMLParseMode,
+            )
+        }
         onContentMessage(
-            initialFilter = CommonMessage<MessageContent>::isNotModeSwitchCommand
+            initialFilter = CommonMessage<MessageContent>::isNotCommand
         ) {
             val text = it.text
 
@@ -35,8 +62,8 @@ suspend fun main(args: Array<String>) {
     }.join()
 }
 
-fun CommonMessage<*>.isNotModeSwitchCommand(): Boolean {
+fun CommonMessage<*>.isNotCommand(): Boolean {
     val content = this.content as? TextContent ?: return true
 
-    return content.textSources.none { it.botCommandTextSourceOrNull()?.command in listOf("scream", "whisper") }
+    return content.textSources.none { it.botCommandTextSourceOrNull() != null }
 }
