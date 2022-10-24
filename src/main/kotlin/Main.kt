2a07c12eb7cc96e4522ef9f87b7feb1f8032b4e8
madhead <siarhei.krukau@gmail.com>
2022-10-24 03:10:54 +0200

    Address some feedback

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,33 +10,30 @@ import dev.inmo.tgbotapi.extensions.behaviour_builder.triggers_handling.onDataCa
 import dev.inmo.tgbotapi.extensions.behaviour_builder.triggers_handling.onMessageDataCallbackQuery
 import dev.inmo.tgbotapi.extensions.utils.botCommandTextSourceOrNull
 import dev.inmo.tgbotapi.extensions.utils.extensions.raw.text
+import dev.inmo.tgbotapi.extensions.utils.types.buttons.flatInlineKeyboard
+import dev.inmo.tgbotapi.extensions.utils.types.buttons.inlineKeyboard
 import dev.inmo.tgbotapi.types.buttons.InlineKeyboardButtons.CallbackDataInlineKeyboardButton
 import dev.inmo.tgbotapi.types.buttons.InlineKeyboardButtons.URLInlineKeyboardButton
-import dev.inmo.tgbotapi.types.buttons.InlineKeyboardMarkup
 import dev.inmo.tgbotapi.types.message.HTMLParseMode
 import dev.inmo.tgbotapi.types.message.abstracts.CommonMessage
+import dev.inmo.tgbotapi.types.message.abstracts.ContentMessage
 import dev.inmo.tgbotapi.types.message.content.MessageContent
 import dev.inmo.tgbotapi.types.message.content.TextContent
+import dev.inmo.tgbotapi.utils.row
 
 private val firstMenu = "<b>Menu 1</b>\n\nA beautiful menu with a shiny inline button.";
 private val secondMenu = "<b>Menu 2</b>\n\nA better menu with even more shiny inline buttons.";
 private val next = CallbackDataInlineKeyboardButton("Next", "next")
 private val back = CallbackDataInlineKeyboardButton("Back", "back")
 private val url = URLInlineKeyboardButton("Tutorial", "https://madhead.me/posts/from-botfather-to-hello-world")
-private val firstMenuMarkup = InlineKeyboardMarkup(
-    listOf(
-        listOf(next)
-    )
-)
-private val secondMenuMarkup = InlineKeyboardMarkup(
-    listOf(
-        listOf(back),
-        listOf(url),
-    )
-)
+private val firstMenuMarkup = flatInlineKeyboard { next }
+private val secondMenuMarkup = inlineKeyboard {
+    row { back }
+    row { url }
+}
 
 suspend fun main(args: Array<String>) {
-    val token = args[0]
+    val token = args.first()
     val bot = telegramBot(token)
     var screaming = false
 
@@ -82,8 +79,7 @@ suspend fun main(args: Array<String>) {
 
         onMessageDataCallbackQuery("back") {
             bot.edit(
-                chatId = it.message.chat.id,
-                messageId = it.message.messageId,
+                message = it.message as ContentMessage<TextContent>,
                 text = firstMenu,
                 parseMode = HTMLParseMode,
                 replyMarkup = firstMenuMarkup,
